eaf8d68f4a27597424b7d414d92736917649ab72
zijian <zijian@C010423722.zipangu.cac.co.jp>
2026-04-16 12:56:18 +0900

    Qusetion01-06完了です。

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
@@ -1,13 +1,30 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_3 {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		// TODO:ここに実装
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int amount = 0; //鉛筆の個数
+		int price = 100; //鉛筆の値段
+
+		System.out.print("鉛筆を購入しますか?\nはい：0、いいえ：1 >");
+		String strbuyFlag = reader.readLine();
+		int buyFlag = Integer.parseInt(strbuyFlag);
+
+		while (buyFlag == 0) {
+			amount++;
+			System.out.print("購入する鉛筆の本数を 1 つ増やしますか？\nはい：0、いいえ：1 >");
+			strbuyFlag = reader.readLine();
+			buyFlag = Integer.parseInt(strbuyFlag);
+		}
+		System.out.println("購入した鉛筆の本数は" + amount + "本です。");
+		System.out.println("購入した鉛筆の合計金額は" + (amount * price) + "円です。");
+
 	}
 
 }
--- java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
@@ -1,12 +1,29 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_4 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		// TODO:ここに実装
+		int amountBooks = 0; //本の冊数
+		int sellFlag = 0; //本を売却するかどうかの判定
+		int bookPrice = 200; //本の値段
+
+		System.out.println("本を売却します。");
+
+		do {
+			amountBooks++;
+			System.out.print("売却する本の冊数を 1 つ増やしますか？\nはい：0、いいえ：1 >");
+			String strsellFlag = reader.readLine();
+			sellFlag = Integer.parseInt(strsellFlag);
+
+		} while (sellFlag == 0);
+		System.out.print("売却する本の冊数は" + amountBooks + "冊です。");
+		System.out.println("売却した本の合計金額は" + (amountBooks * bookPrice) + "円です。");
 
 	}
 
